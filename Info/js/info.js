c5cf9c39feb47f8f4ca39254a6ab090c834a6b8b
Miiaroma <miia.romanainen@gmail.com>
2019-09-25 20:22:49 +0300

    Modified views.

--- Info/js/info.js
+++ Info/js/info.js
@@ -5,7 +5,8 @@ var city= "Lappeenranta";
 document.getElementById("map").src='https://www.google.com/maps?q=' + address + city + '&output=embed';
 }
 
-function setAddress() {
+function setAddress()
+ {
     var address = document.getElementById("address").value;
     var city = document.getElementById("city").value;
     document.getElementById("map").src='https://www.google.com/maps?q=' + address + city + '&output=embed';
