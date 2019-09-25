c81833c9c7203f3de3a16287b3a48882a108a5ce
Miiaroma <miia.romanainen@gmail.com>
2019-09-25 15:44:18 +0300

    Map added.

---
+++ Info/js/info.js
@@ -0,0 +1,12 @@
+window.onload = function()
+{
+var address = "Yliopistonkatu 36";
+var city= "Lappeenranta";
+document.getElementById("map").src='https://www.google.com/maps?q=' + address + city + '&output=embed';
+}
+
+function setAddress() {
+    var address = document.getElementById("address").value;
+    var city = document.getElementById("city").value;
+    document.getElementById("map").src='https://www.google.com/maps?q=' + address + city + '&output=embed';
+}
