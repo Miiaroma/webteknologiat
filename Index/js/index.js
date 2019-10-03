cb09838d06d10f85455e502236ccb5ee658906c6
Miiaroma <miia.romanainen@gmail.com>
2019-10-03 13:42:18 +0300

    Refactored increaseNumber and decreaseNumber functions.

--- Index/js/index.js
+++ Index/js/index.js
@@ -2,45 +2,50 @@ window.onload = function () {
     init();
 };
 
-function getRandomInt(min,max) 
-{
+function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function init(){
+function init() {
     //console.log(getRandomInt(1,10));
-    document.getElementById("valuea").value = getRandomInt(1,10);
-    document.getElementById("valueb").value = getRandomInt(1,10);     
+    document.getElementById("valuea").value = getRandomInt(1, 10);
+    document.getElementById("valueb").value = getRandomInt(1, 10);
 }
 
-function decreaseNumber() {
+/*function decreaseNumber() {
     var x = document.getElementById("valuea").value;
     x--;
-    var y = x;    
-    document.getElementById("valuea").value = x;     
-}
+    var y = x;
+    document.getElementById("valuea").value = x;
+}*/
 
-function decreaseNumberB()
-{
-    var x = document.getElementById("valueb").value;
+function decreaseNumber(elementId) {
+    
+    var x = document.getElementById(elementId).value;
     x--;
     var y = x;    
-    document.getElementById("valueb").value = x; 
+    document.getElementById(elementId).value = x;     
 }
 
-function increaseNumber() {
-    var x = document.getElementById("valuea").value;
+/*function decreaseNumberB() {
+    var x = document.getElementById("valueb").value;
+    x--;
+    var y = x;
+    document.getElementById("valueb").value = x;
+}*/
+
+function increaseNumber(elementName) {
+    var x = document.getElementById(elementName).value;
     x++;
-    var y = x;    
-    document.getElementById("valuea").value = x;    
+    var y = x;
+    document.getElementById(elementName).value = x;
 }
 
-function increaseNumberB()
-{
+/*function increaseNumberB() {
     var x = document.getElementById("valueb").value;
     x++;
-    var y = x;    
-    document.getElementById("valueb").value = x; 
-}
+    var y = x;
+    document.getElementById("valueb").value = x;
+}*/
