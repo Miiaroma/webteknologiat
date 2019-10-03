11b0d0b3d08d2b351fc58a15be313daea33967ae
Miiaroma <miia.romanainen@gmail.com>
2019-10-03 16:09:23 +0300

    Calculator added.

--- Index/js/index.js
+++ Index/js/index.js
@@ -22,11 +22,11 @@ function init() {
 }*/
 
 function decreaseNumber(elementId) {
-    
+
     var x = document.getElementById(elementId).value;
     x--;
-    var y = x;    
-    document.getElementById(elementId).value = x;     
+    var y = x;
+    document.getElementById(elementId).value = x;
 }
 
 /*function decreaseNumberB() {
@@ -49,3 +49,38 @@ function increaseNumber(elementName) {
     var y = x;
     document.getElementById("valueb").value = x;
 }*/
+
+function calculator() {
+    var x = document.getElementById("valuea").value;
+    var y = document.getElementById("valueb").value;
+    document.getElementById("result").value = operate(x, y);
+}
+
+function operate(x, y) {
+    if (select == '+') {
+        return x + y;
+    }
+
+    else if (select == '-') {
+        return x - y;
+    }
+
+    else if (select == '*') {
+        return x * y;
+    }
+    else if (select == '/') {
+        return x / y;
+    }
+}
+
+function validate(value) 
+{
+    if (value == null || value == "") {
+        alert("Required Field");
+        return 0;
+    } else if (isNaN(value)) {
+        alert("Must be a Number Field");
+        return 0;
+    } else return value;
+}
+
