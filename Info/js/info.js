fdc3ec046a940393d64187d52914a6d30f5b3214
Miiaroma <miia.romanainen@gmail.com>
2019-11-04 10:57:50 +0200

    Refactoring class change.

--- Info/js/info.js
+++ Info/js/info.js
@@ -1,21 +1,21 @@
-window.onload = function()
-{
-var address = "Yliopistonkatu 36";
-var city= "Lappeenranta";
-document.getElementById("map").src='https://www.google.com/maps?q=' + address + city + '&output=embed';
+window.onload = function () {
+    var address = "Yliopistonkatu 36";
+    var city = "Lappeenranta";
+    document.getElementById("map").src = 'https://www.google.com/maps?q=' + address + city + '&output=embed';    
 }
 
-function setAddress()
- {
+function setAddress() {
     var address = document.getElementById("address").value;
     var city = document.getElementById("city").value;
-    document.getElementById("map").src='https://www.google.com/maps?q=' + address + city + '&output=embed';
+    document.getElementById("map").src = 'https://www.google.com/maps?q=' + address + city + '&output=embed';
 }
 
-$("#grid").click(function () {    
-    $("grid-container").removeClass("grid-container noClass").addClass("grid");
+$("#grid").click(function () {
+    console.log("")
+    $("#gridview").removeClass("list-container").addClass("grid-container");
 });
 
 $("#list").click(function () {
-    $("grid-container").removeClass("display grid").addClass("block");
+    console.log("listanäkymä");
+    $("#gridview").removeClass("grid-container").addClass("list-container");
 });
