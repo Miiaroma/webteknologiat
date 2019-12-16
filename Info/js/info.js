d6ea4c74ade15bc951b01249774369a4f0cb443a
Miiaroma <miia.romanainen@gmail.com>
2019-12-16 15:14:32 +0200

    Server programming added.

--- Info/js/info.js
+++ Info/js/info.js
@@ -1,5 +1,6 @@
 var index;
 var url = "http://api.icndb.com/jokes/random/1";
+var listUrl = "http://localhost:8080/tasks";
 var data;
 var success;
 
@@ -25,9 +26,9 @@ $("#btn").click(function () {
 
             $.each(response.value, function (index, jokeObject) {
                 //console.log(jokeObject.joke)
-                $("#jokeInfo").append(jokeObject.joke);               
-            }); 
-            
+                $("#jokeInfo").append(jokeObject.joke);
+            });
+
             index++
             $("#gif").hide();
             //console.log(index);
@@ -38,7 +39,8 @@ $("#btn").click(function () {
             $("#gif").hide();
             Console.log("Error msg")
         },
-    });    
+    });
+
 
     /* $.getJSON(urlNorris,
          function (result) {
@@ -54,6 +56,63 @@ $("#btn").click(function () {
      });*/
 
 });
+
+$("#td").click(function () {
+    console.log("xxx");
+    $("#gif").show();
+
+    $.ajax({
+        type: "GET",
+        url: listUrl,
+        dataType: "json",
+        success: function (response) {
+            $("#gif").hide();
+            //$("#btn").attr("disabled", "true");
+            console.log(response);
+
+            $.each(response, function (index, value) {
+                console.log(value);
+                $("#todo").append(value.id + ' ' + value.title);
+                $('<input type="button" id="tdBtn" onClick="moreInfo(' + index + ')"/>').appendTo("#todo");
+                $("#todo").append("<br>");
+            });
+
+            /*index++
+            $("#gif").hide();
+            //console.log(index);
+            $("aside").html('Counter:' + index);*/
+
+        },
+        error: function (errorResponse) {
+            $("#gif").hide();
+            console.log("Error msg")
+        },
+    });
+});
+
+function moreInfo(id) {
+    console.log(id);
+    id++;
+    var itemUrl = "http://localhost:8080/task/" + id;    
+
+    console.log(itemUrl);
+    $.ajax({
+        type: "GET",
+        url: itemUrl,
+        dataType: "json",
+        success: function (response) {
+
+            console.log(response);
+            $("#todo").append(response.id +""+response.title+""+response.description+""+response.complete+""+response.cancelled);
+            $("#todo").append("<br>");
+
+        },
+        error: function (errorResponse) {
+            console.log("Error msg")
+        },
+    });
+}
+
 
 window.onload = function () {
     var address = "Yliopistonkatu 36";
@@ -77,4 +136,3 @@ $("#list").click(function () {
     console.log("listanäkymä");
     $("#gridview").removeClass("grid-container").addClass("list-container");
 });
-
